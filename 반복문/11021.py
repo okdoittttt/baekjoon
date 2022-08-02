9269d226a5be1a8f47dec95d0af11886b31de318
밥장군 <sonokmoo@gmail.com>
2022-08-03 00:30:28 +0900

    조건문

--- 반복문/11021.py
+++ 반복문/11021.py
@@ -1,7 +1,6 @@
 import sys
-
 t = int(input())
 
-for i in range(t):
+for i in range(1, t+1):
     a,b = map(int, sys.stdin.readline().split())
-    print(a+b)
+    print("Case #{}: {}".format(i, a+b))
